0d429f7072aa79bb78920a7de1ab2de1ac2c15c9
paether <petersocial@icloud.com>
2022-04-19 12:12:38 +0200

    ssl forcing added

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 8800;
 const app = express();
 
 app.use(helmet());
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
 
 app.use(express.static(path.join(__dirname, "/client/build")));
 
